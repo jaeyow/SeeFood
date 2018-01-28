cac713d4bcdf04da4830b2cd11b9d07d59bf9252
Lorence Lim <lorence@ingenuity.ph>
2018-01-28 21:58:06 +0800

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Lorence Lim on 28/01/2018.
+//  Copyright © 2018 Lorence Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
